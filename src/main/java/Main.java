38c5b48e66b9ab34a502927909f6429c0282fc39
Varfolomeev Alexander <vas.aleks.v@gmail.com>
2024-01-22 21:39:58 +0300

    fix: split main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,23 +16,35 @@ public class Main {
         this.config = config;
     }
 
-    public void start() {
+    /**
+     * Run {@link Analyzer} with given options
+     *
+     * @return defects or null if error occurs
+     */
+    public DefectStorage start() {
         Analyzer analyzer = new Analyzer(config);
 
         analyzer.printProblems();
 
         if (failOnErrors && analyzer.hasProblems()) {
-            System.exit(1);
+            return null;
         }
 
         DefectStorage defectStorage = analyzer.start();
         defectStorage.printDefects();
 
         if (failOnErrors && defectStorage.defects().values().stream().mapToLong(List::size).sum() != 0) {
-            System.exit(1);
+            return null;
         }
+
+         return defectStorage;
     }
 
+    /**
+     * Parse command line arguments and runs analyzer
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         Options options = new Options();
         options.addOption("a", "ast", false, "Print AST.");
@@ -53,7 +65,13 @@ public class Main {
                 String path = cmd.getOptionValue('i', ".");
                 Config analyzerConfig = new Config(printAST, Path.of(path));
 
-                new Main(failOnErrors, analyzerConfig).start();
+                DefectStorage defectStorage = new Main(failOnErrors, analyzerConfig).start();
+
+                if (defectStorage == null) {
+                    System.exit(1);
+                } else {
+                    defectStorage.printDefects();
+                }
             }
         } catch (ParseException e) {
             System.err.println(e.getMessage());
